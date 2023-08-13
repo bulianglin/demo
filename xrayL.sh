34ad61ab3e188f222a0b260f61cdac81782b200b
bulianglin <78075785+bulianglin@users.noreply.github.com>
2023-08-13 19:13:00 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -87,7 +87,7 @@ config_xray() {
 	done
 	echo -e "$config_content" >/etc/xrayL/config.toml
 	systemctl restart xrayL.service
-	systemctl status xrayL.service
+	systemctl --no-pager status xrayL.service
 	echo ""
 	echo "生成 $config_type 配置完成"
 	echo "起始端口:$START_PORT"
